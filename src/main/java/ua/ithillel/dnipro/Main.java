db7e159ab1345e7452079a94f85aaac936133497
Andrey Pritula <a.s.pritula@icloud.com>
2018-05-14 20:53:29 +0300

    Home work was done

--- src/main/java/ua/ithillel/dnipro/Main.java
+++ src/main/java/ua/ithillel/dnipro/Main.java
@@ -1,39 +1,67 @@
 package ua.ithillel.dnipro;
 
-import com.mysql.cj.jdbc.Driver;
 import java.sql.*;
-
 
 public class Main {
 
-    private static Connection con = null;
-    private static String username = "root";
-    private static String password = "RdJOsdi14";
-    private static String URL = "jdbc:mysql://127.0.0.1:3306/lesson_14";
+    private static Connection connection = null;
+    private static String username = "student";
+    private static String password = "password";
+    private static String URL = "jdbc:mysql://localhost:3306/lesson_14"+
+            "?verifyServerCertificate=false"+
+            "&useSSL=false"+
+            "&requireSSL=false"+
+            "&useLegacyDatetimeCode=false"+
+            "&amp"+
+            "&serverTimezone=UTC";
 
     public static void main(String[] args) throws SQLException {
         try{
-            DriverManager.registerDriver(new com.mysql.jdbc.Driver());//Загружаем драйвер
-            con = DriverManager.getConnection(URL, username, password);          //соединяемся
-            if(con!=null) System.out.println("Connection Successful !\n");
-            if (con==null) System.exit(0);
-            Statement st = con.createStatement(); //Statement позволяет отправлять запросы базе данных
-            ResultSet rs = st.executeQuery("select * from TABLENAME");
-            //ResultSet получает результирующую таблицу
-            int x = rs.getMetaData().getColumnCount();//Resultset.getMetaData()получаем информацию результирующей т
-            while(rs.next()){
-                for(int i=1; i<=x;i++){
-                    System.out.print(rs.getString(i) + "\t");
-                }
-                System.out.println();
-            }
-            System.out.println();
-            if(rs!=null)rs.close();
-            if(st!=null)st.close();
-            if(con!=null)con.close();
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());//Загружаем драйвер
+            connection = DriverManager.getConnection(URL, username, password);          //соединяемся
+            if(connection!=null) System.out.println("Connection is successful !\n");
+            if (connection==null) System.exit(0);
+
         } catch (SQLException e){
-            System.out.println("SQL connection failed");
+            System.out.println("SQL connection is failed");
         }
 
+        //Statement позволяет отправлять запросы базе данных
+        Statement st = connection.createStatement();
+
+        //Create
+        st.execute("INSERT INTO users(user_name, pass) VALUE ('vasya', '12345')");
+        st.execute("INSERT INTO users(user_name, pass) VALUE ('petya', '54321')");
+        st.execute("INSERT INTO users(user_name, pass) VALUE ('vasya', '1123123123123')");
+
+        //Update
+        try {
+            st.executeUpdate("UPDATE users SET pass = '777' WHERE user_name = 'petya'");
+        } catch (SQLException e){
+            System.out.println("update is failed");
+        }
+
+        //Delete
+        try {
+            st.executeUpdate("DELETE FROM users WHERE id ='10'");
+        } catch (SQLException e){
+            System.out.println("Something's wrong here");
+        }
+
+        //ResultSet получает результирующую таблицу
+        ResultSet rs = st.executeQuery("select * from users");
+        //Resultset.getMetaData()получаем информацию результирующей т
+        int x = rs.getMetaData().getColumnCount();
+        while(rs.next()){
+            for(int i=1; i<=x;i++){
+                System.out.print(rs.getString(i) + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        if(rs!=null)rs.close();
+        if(st!=null)st.close();
+        if(connection!=null)connection.close();
     }
 }
